14f6571c66806fa395264c5caf9021f959ca0d2a
Anika <anika_0115@yahoo.com>
2020-12-01 19:05:14 -0500

    Add project files.

---
+++ DB Project/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DB_Project.Models;
+using System.Data.SqlClient;
+
+namespace DB_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<Classes> classes = new List<Classes>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            con.ConnectionString = DB_Project.Properties.Resources.ConnectionString;
+        }
+
+        public IActionResult Index()
+        {
+            FetchData();
+            return View(classes);
+        }
+
+        private void FetchData()
+        {
+            if(classes.Count > 0)
+            {
+                classes.Clear();
+            }
+
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM Classes";
+                dr = com.ExecuteReader();
+                while(dr.Read())
+                {
+                    classes.Add(new Classes() {Class_Title = dr["Class_Title"].ToString()
+                    ,Class_Code = dr["Class_Code"].ToString()
+                    ,Class_Description = dr["Class_Description"].ToString()
+
+                        });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DB Project/Models/Classes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB_Project.Models
+{
+    public class Classes
+    {
+        public string Class_Title { get; set; }
+        public string Class_Code { get; set; }
+        public string Class_Description { get; set; }
+    }
+}
